b32a4b645138e4705ef4582d76cba33b4f014c65
Waleed <waleedbro994@gmail.com>
2025-12-22 16:55:57 +0300

    customaizing search views & adding validation

--- odoo/custom_addons/real_estate/models/estate_property.py
+++ odoo/custom_addons/real_estate/models/estate_property.py
@@ -4,13 +4,13 @@ from odoo import models,fields
 class estateProperty(models.Model):
     _name = 'estate.property'
 
-    name = fields.Char()
+    name = fields.Char(required=1)
     description = fields.Text()
-    postcode = fields.Char()
+    postcode = fields.Char(required=1)
     date_availability = fields.Date()
     expected_price = fields.Float()
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float()      
+    bedrooms = fields.Integer(required=True)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
