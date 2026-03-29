a26e3ae3755639de5afba78f2b5c957040c773de
Waleed <waleedbro994@gmail.com>
2026-03-29 22:34:53 +0300

    new changes

--- odoo/custom_addons/real_estate/reports/property_xlsx_report.py
+++ odoo/custom_addons/real_estate/reports/property_xlsx_report.py
@@ -16,3 +16,19 @@ class PropertyXlsxReport(http.Controller):
         header_format = workbook.add_format({'bold' : True, 'bg_color' : '#D3D3D3', 'border' : 1, 'align' : 'center'})
 
         headers = ['Name', 'Postcode', 'Bedrooms', 'Selling Price']
+
+        for col, header in enumerate(headers):
+            worksheet.write(0,col,header,header_format)
+
+        workbook.close()
+        output.seek(0)
+
+        file_name = 'Property Report.xlsx'
+
+        return request.make_response(
+            output.getvalue(),
+            headers= [
+                ('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'),
+                ('Content-Disposition', f'attachment; filename={file_name}')
+            ]
+        )
